6bb179bf7810af1ee64382fa82151c05cb5c5c3e
Gabriel Jorge Tavares <gabriel.tavaresramos@gmail.com>
2019-09-30 10:32:47 -0300

    Remoção de propriedades não usadas no componente ControlsManager

--- bicho-ufc-rampage-project/Assets/_Project/Scripts/ControlsManager.cs
+++ bicho-ufc-rampage-project/Assets/_Project/Scripts/ControlsManager.cs
@@ -1,20 +1,15 @@
 ﻿using Magrathea.BichoUFCRampage.Controls;
+using Magrathea.BichoUFCRampage.Inputs;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ControlsManager : MonoBehaviour {
 
-	public Vector2 velocidadePersonagem;// Força de movimento em X e Y
 	public Vector2 velocidadeDash;// velocidades do dash em x e y
-	public float velocidadeMaximaX;// Velocidade máxima no eixo X
-	public LayerMask layerMask;// Layers em que o boneco reage
-	public bool moverNoAr = false;// Se é possível mover pro lado durante o pulo
 	public ControleDeScore controladorDeScore;// usado para contar pontos para quando o jogador aperta "A" ou "D" alternado
 	public Text textoDash;
-	public Transform groundCheck, groundCheck1;// Objeto que ajuda a checar se o boneco está no chão
 
-	private bool podePular = false;// Controla se o bonco está no chão. se estiver pode pular
-	private Rigidbody2D rb2d;
+	private Rigidbody2D _rigidbody;
 	private bool proxBotao = true;// Controla a alternância entre os botões de entrada
 	private Animator anim;// Controle de animação do personagem
 	private bool parar = false;
@@ -29,9 +24,10 @@ public class ControlsManager : MonoBehaviour {
     private IMovableRight _rightMover;
     private IJumpable _jumpper;
     private IGroundable _groundChecker;
+    private IInputManager _inputManager;
 
 	void Awake () {
-		this.rb2d = GetComponent <Rigidbody2D> ();
+		this._rigidbody = GetComponent <Rigidbody2D> ();
 		this.anim = GetComponent <Animator> ();
 		textoDash.enabled = false;
 
@@ -43,17 +39,10 @@ public class ControlsManager : MonoBehaviour {
         _groundChecker.Layer = groundLayer;
         _groundChecker.LeftChecker = leftChecker;
         _groundChecker.RightChecker = rightChecker;
+        _inputManager = GetComponent<InputManager>();
 	}
 
-	/*
-	 * No Update apenas verificamos se os botões foram apertados. Como o Update tem mais frames,
-	 * isso faz com que as chances de detectar se um botão foi pressionado maiores e torna o jogo 
-	 * mais confortável.
-	*/
 	void Update () {
-        // Detecta se pode pular
-        this.podePular = _groundChecker.IsGrounded();
-
 		if (Input.GetKeyDown (KeyCode.A) && this.proxBotao) {
 			this.a = true;
 			this.proxBotao = false;
@@ -62,14 +51,14 @@ public class ControlsManager : MonoBehaviour {
 			this.proxBotao = true;
 		} 
 
-		if (Input.GetKeyDown (KeyCode.Space) && this.podePular) {
+		if (Input.GetKeyDown (KeyCode.Space) && _groundChecker.IsGrounded()) {
 				this.p = true;
 		}
 	}
 
 	void FixedUpdate () {
 		// Desnecessário depois do GroundChecker
-		this.anim.SetFloat ("Correndo", Mathf.Abs (this.rb2d.velocity.x));
+		this.anim.SetFloat ("Correndo", Mathf.Abs (this._rigidbody.velocity.x));
 
 		if (!this.parar) {
 			// Detecta se os botões estão sendo apertados
@@ -86,7 +75,7 @@ public class ControlsManager : MonoBehaviour {
 
 			// Pulo
 			if (this.p) {
-				if (this.podePular) {
+				if (_groundChecker.IsGrounded()) {
 					_jumpper.JumpNow();
 					this.p = false;
 				}
@@ -108,8 +97,8 @@ public class ControlsManager : MonoBehaviour {
 	void Bater () {
 		this.anim.SetTrigger ("Bater");
 		Vector3 vx = Vector3.zero;
-		this.rb2d.velocity = vx;
-		vx.x = -velocidadePersonagem.x;
-		this.rb2d.velocity = vx;
+		this._rigidbody.velocity = vx;
+		//vx.x = -velocidadePersonagem.x;
+		this._rigidbody.velocity = vx;
 	}
 }
